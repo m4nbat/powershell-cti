4639f0a1831411e096d45e5c060a22e38aa8530a
Gavin Knapp <g.r.j.knapp@gmail.com>
2023-09-08 23:48:58 +0100

    Update systembc-panel-check.ps1

--- systembc-panel-check.ps1
+++ systembc-panel-check.ps1
@@ -1,8 +1,7 @@
-# Usage Instructions for Option 2:
-# Ensure you have a CSV file named 'ips.csv' in the same directory as this script.
-# The CSV file should have a column named 'IPAddress' containing the list of IP addresses.
+# Ensure you have a text file named 'ips.txt' in the same directory as this script.
+# The text file should have one IP address per line.
 
-$choice = Read-Host 'Enter 1 to input IP address/range manually or 2 to read from ips.csv'
+$choice = Read-Host 'Enter 1 to input IP address/range manually or 2 to read from ips.txt'
 
 # Function to test connection
 function Test-HttpConnection {
@@ -11,19 +10,23 @@ function Test-HttpConnection {
     )
     $urls = @("http://$ip/systembc/password.php", "http://$ip/www/systembc/password.php")
     $success = $false
+    $expectedContent = '<pre align=center><form method=post>Password: <input type=password name=pass><input type=submit value=">>"></form></pre>'
+    
     foreach ($url in $urls) {
         try {
-            $response = Invoke-WebRequest -Uri $url -TimeoutSec 2 -ErrorAction SilentlyContinue
-            if ($response.StatusCode -eq 200 -and $response.BaseResponse.ResponseUri.AbsoluteUri -eq $url) {
+            $response = Invoke-WebRequest -Uri $url -TimeoutSec 1 -ErrorAction SilentlyContinue
+            if ($response.StatusCode -eq 200 -and $response.BaseResponse.ResponseUri.AbsoluteUri -eq $url -and $response.Content.Contains($expectedContent)) {
                 $success = $true
                 break
             }
         } catch {
             # Do nothing on error
         }
+        # Introducing a delay of 0 seconds between each web request
+        Start-Sleep -Seconds 0
     }
     if ($success) {
-        Write-Output "Success: $ip"
+        Write-Output "Success: $url"
     } else {
         Write-Output "Fail - no panel: $ip"
     }
@@ -43,9 +46,12 @@ if ($choice -eq '1') {
         Test-HttpConnection -ip $ipInput
     }
 } elseif ($choice -eq '2') {
-    $ips = Import-Csv -Path .\ips.csv
-    foreach ($ip in $ips.IPAddress) {
+    # Reading IP addresses from ips.txt
+    $ips = Get-Content -Path .\ips.txt
+    foreach ($ip in $ips) {
         Test-HttpConnection -ip $ip
+        # Introducing a delay of 2 seconds between each IP address check
+        Start-Sleep -Seconds 2
     }
 } else {
     Write-Output 'Invalid choice'
